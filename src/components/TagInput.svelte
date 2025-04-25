5486c0a9d8de1342681a13312058201f4ae1f3f2
Chris Parent <chrisnparent@gmail.com>
2025-04-25 15:54:01 -0400

    fix dancing tag inputs when multiple are active

--- src/components/TagInput.svelte
+++ src/components/TagInput.svelte
@@ -5,25 +5,29 @@
     export let remove = () => {};
 
     let active = false;
+    let shouldFocus = false;
     let control;
 
-    function handleApply() {
-        active = false;
-        if (!content) content = "click to edit";
+    let activate = () => {
+        active = true;
+        shouldFocus = true;
     }
 
     afterUpdate(() => {
-        if (control) control.focus();
+        if (!control || !shouldFocus) return; 
+        
+        control.focus();
+        shouldFocus = false;
     });
 </script>
 
 {#if active}
     <div class="mt-1 d-flex flex-grow-1">
         <input bind:this={control} class="form-control" bind:value={content}>
-        <button class="btn btn-light border" on:click={handleApply}>&check;</button>
+        <button class="btn btn-light border" on:click={() => active = false}>&check;</button>
         <button class="btn btn-danger border" on:click={() => remove()}>&cross;</button>
     </div>
 {:else}
-    <button class="text-left flex-grow-1 btn btn-light mt-1" on:click={() => active = true}>{content}</button>
+    <button class="text-left flex-grow-1 btn btn-light mt-1" style="min-height: 2.2em" on:click={() => activate()}>{content}</button>
 {/if}
 
